4bc0fab3fc9f09165acd2177996823bd9ace8ab5
Ivan Lauryu <urpretti@gmail.com>
2025-05-12 23:48:41 +0300

    first commit

--- src/backend/OfficesManagement/src/External/OfficesManagement.Infrastructure/Persistence/Configurations/MongoSettings.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace OfficesManagement.Infrastructure.Persistence.Configurations;
-public class MongoSettings
-{
-    public string ConnectionString { get; set; } = null!;
-    public string DatabaseName { get; set; } = null!;
-}
