3312ff46c73a9155c71e0b8e1a73756be8b756a9
Ivan Lauryu <urpretti@gmail.com>
2025-06-05 17:33:54 +0300

    changes4

--- src/backend/ProfilesManagement/src/Application/ProfilesManagement.Application/UseCases/Patient/DeletePatientHandler.cs
+++ src/backend/ProfilesManagement/src/Application/ProfilesManagement.Application/UseCases/Patient/DeletePatientHandler.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using MyMediator.Interfaces;
+﻿using MyMediator.Interfaces;
 using ProfilesManagement.Application.Common.Interfaces.IRepositories;
 
 namespace ProfilesManagement.Application.UseCases.Patient
 {
-    public record DeletePatientRequest(Guid Id) : IRequest<Unit>;
+    public record DeletePatientRequest(Guid Id) : IRequest<bool>;
 
-    public class DeletePatientHandler : IRequestHandler<DeletePatientRequest, Unit>
+    public class DeletePatientHandler : IRequestHandler<DeletePatientRequest, bool>
     {
         private readonly IPatientRepository _repository;
 
@@ -18,14 +14,18 @@ namespace ProfilesManagement.Application.UseCases.Patient
             _repository = repository;
         }
 
-        public async Task<Unit> HandleAsync(DeletePatientRequest request, CancellationToken cancellationToken)
+        public async Task<bool> HandleAsync(DeletePatientRequest request, CancellationToken cancellationToken)
         {
             var existing = await _repository.GetByIdAsync(request.Id);
+
             if (existing == null)
+            {
                 throw new KeyNotFoundException($"Patient with Id = {request.Id} not found.");
+            }
 
             await _repository.DeleteAsync(existing);
-            return Unit.Value; 
+
+            return true;
         }
     }
 }
