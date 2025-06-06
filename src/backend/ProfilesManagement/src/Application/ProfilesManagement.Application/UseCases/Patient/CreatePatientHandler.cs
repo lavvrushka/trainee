ebcc9fa63725cb3ea165001f5605344bb72436bb
Ivan Lauryu <urpretti@gmail.com>
2025-06-07 01:17:39 +0300

    changes10

--- src/backend/ProfilesManagement/src/Application/ProfilesManagement.Application/UseCases/Patient/CreatePatientHandler.cs
+++ src/backend/ProfilesManagement/src/Application/ProfilesManagement.Application/UseCases/Patient/CreatePatientHandler.cs
@@ -3,15 +3,7 @@ using ProfilesManagement.Application.Common.Interfaces.IRepositories;
 using ProfilesManagement.Application.DTOs;
 namespace ProfilesManagement.Application.UseCases.Patient;
 
-public record CreatePatientRequest(
-        string Name,
-        string Description,
-        decimal Price,
-        int Quantity,
-        bool IsAvailable,
-        string ImageData,
-        string ImageType
-    ) : IRequest<PatientDto>;
+public record CreatePatientRequest() : IRequest<PatientDto>;
 
 public class CreatePatientHandler : IRequestHandler<CreatePatientRequest, Guid>
 {
@@ -27,12 +19,9 @@ public class CreatePatientHandler : IRequestHandler<CreatePatientRequest, Guid>
         var dto = request.Patient;
         var patient = new Patient
         {
-            Id = Guid.NewGuid(),
             FirstName = dto.FirstName,
             LastName = dto.LastName,
             MiddleName = dto.MiddleName,
-            AccountId = dto.AccountId,
-            ImageId = dto.ImageId
         };
 
         await _repository.AddAsync(patient);
