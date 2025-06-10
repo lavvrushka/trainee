f74296f10e70bad3d9b7f489442f6dc783d71415
Ivan Lauryu <urpretti@gmail.com>
2025-06-10 14:06:16 +0300

    add exceptions

---
+++ src/backend/ProfilesManagement/src/Application/ProfilesManagement.Application/Common/Exeptions/ValidationException.cs
@@ -0,0 +1,12 @@
+﻿namespace ProfilesManagement.Application.Common.Exeptions;
+
+public class ValidationEventAppException : Exception
+{
+    public IReadOnlyDictionary<string, string[]> Errors { get; }
+
+    public ValidationEventAppException(IReadOnlyDictionary<string, string[]> errors)
+        : base("One or more validation errors occurred")
+    {
+        Errors = errors;
+    }
+}
---
+++ src/backend/ProfilesManagement/src/Application/ProfilesManagement.Application/Common/Validation/DoctorValidators/CreateDoctorRequestValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using ProfilesManagement.Application.UseCases.DoctorUseCases;
+namespace ProfilesManagement.Application.Common.Validation.DoctorValidators;
+
+public class CreateDoctorRequestValidator : AbstractValidator<CreateDoctorRequest>
+{
+    public CreateDoctorRequestValidator()
+    {
+        RuleFor(r => r.FirstName)
+            .NotEmpty()
+            .MaximumLength(100);
+
+        RuleFor(r => r.LastName)
+            .NotEmpty()
+            .MaximumLength(100);
+
+        RuleFor(r => r.MiddleName)
+            .MaximumLength(100);
+
+        RuleFor(r => r.AccountId)
+            .NotEmpty();
+
+        RuleFor(r => r.CareerStartYear)
+            .LessThanOrEqualTo(DateTime.UtcNow)
+            .WithMessage("Career start year cannot be in the future.");
+
+        RuleFor(r => r.Status)
+            .IsInEnum();
+    }
+}
