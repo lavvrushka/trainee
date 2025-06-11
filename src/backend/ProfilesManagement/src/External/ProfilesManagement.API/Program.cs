61774e15bc004e7a2bdd8038b63764b6661662f1
Ivan Lauryu <urpretti@gmail.com>
2025-06-12 02:57:15 +0300

    changes12

--- src/backend/ProfilesManagement/src/External/ProfilesManagement.API/Program.cs
+++ src/backend/ProfilesManagement/src/External/ProfilesManagement.API/Program.cs
@@ -1,6 +1,71 @@
+﻿using ProfilesManagement.API.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddValidationServices();
+builder.Services.AddJwtAuthentication(builder.Configuration);
+builder.Services.AddCustomMiddlewares();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Ââåäèòå âàø Bearer òîêåí. Ïðèìåð: 'Bearer abcdef12345'"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReact", policy =>
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowCredentials());
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors("AllowReact");
+app.UseCors("AllowProductService");
 
+app.UseMiddleware<GlobalExceptionMiddleware>();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 app.Run();
