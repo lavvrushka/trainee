7b17fd5d2e4f4e56e65d764aa3ec6dfefa22c888
Ivan Lauryu <urpretti@gmail.com>
2025-06-12 13:13:38 +0300

    added controllers

--- src/backend/ProfilesManagement/src/External/ProfilesManagement.API/Controllers/PatientsController.cs
+++ src/backend/ProfilesManagement/src/External/ProfilesManagement.API/Controllers/PatientsController.cs
@@ -1,6 +1,32 @@
-﻿namespace ProfilesManagement.API.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyMediator.Interfaces;
+using ProfilesManagement.API.Filters;
+namespace ProfilesManagement.API.Controllers;
+
+[Route("api/patient")]
+[ApiController]
+[Authorize]
+public class PatientController : ControllerBase
 {
-    public class PatientsController
+    private readonly IMediator _mediator;
+    public PatientController(IMediator mediator) => _mediator = mediator;
+
+    // POST api/patient/create
+    [HttpPost("create")]
+    [ValidateModel]
+    public async Task<ActionResult<Guid>> Create([FromBody] CreatePatientRequest request)
     {
+        var id = await _mediator.Send(request);
+        return CreatedAtAction(null, id);
+    }
+
+    // PUT api/patient/update
+    [HttpPut("update")]
+    [ValidateModel]
+    public async Task<IActionResult> Update([FromBody] UpdatePatientRequest request)
+    {
+        await _mediator.Send(request);
+        return Ok();
     }
 }
--- src/backend/ProfilesManagement/src/External/ProfilesManagement.API/Controllers/ReceptionistsController.cs
+++ src/backend/ProfilesManagement/src/External/ProfilesManagement.API/Controllers/ReceptionistsController.cs
@@ -1,6 +1,72 @@
 ﻿namespace ProfilesManagement.API.Controllers
 {
-    public class ReceptionistsController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PatientsController : ControllerBase
     {
+        private readonly IRequestHandler<CreatePatientRequest, Guid> _createHandler;
+        private readonly IRequestHandler<UpdatePatientRequest, Unit> _updateHandler;
+
+        public PatientsController(
+            IRequestHandler<CreatePatientRequest, Guid> createHandler,
+            IRequestHandler<UpdatePatientRequest, Unit> updateHandler)
+        {
+            _createHandler = createHandler;
+            _updateHandler = updateHandler;
+        }
+
+        // POST: api/patients
+        [HttpPost]
+        public async Task<ActionResult<Guid>> Create([FromBody] CreatePatientRequest request)
+        {
+            var id = await _createHandler.Handle(request, HttpContext.RequestAborted);
+            return CreatedAtAction(null, id);
+        }
+
+        // PUT: api/patients/{id}
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] UpdatePatientRequest request)
+        {
+            if (id != request.Id)
+                return BadRequest("Route id and body id do not match.");
+
+            await _updateHandler.Handle(request, HttpContext.RequestAborted);
+            return NoContent();
+        }
+    }
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReceptionistsController : ControllerBase
+    {
+        private readonly IRequestHandler<CreateReceptionistRequest, Guid> _createHandler;
+        private readonly IRequestHandler<UpdateReceptionistRequest, Unit> _updateHandler;
+
+        public ReceptionistsController(
+            IRequestHandler<CreateReceptionistRequest, Guid> createHandler,
+            IRequestHandler<UpdateReceptionistRequest, Unit> updateHandler)
+        {
+            _createHandler = createHandler;
+            _updateHandler = updateHandler;
+        }
+
+        // POST: api/receptionists
+        [HttpPost]
+        public async Task<ActionResult<Guid>> Create([FromBody] CreateReceptionistRequest request)
+        {
+            var id = await _createHandler.Handle(request, HttpContext.RequestAborted);
+            return CreatedAtAction(null, id);
+        }
+
+        // PUT: api/receptionists/{id}
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] UpdateReceptionistRequest request)
+        {
+            if (id != request.Id)
+                return BadRequest("Route id and body id do not match.");
+
+            await _updateHandler.Handle(request, HttpContext.RequestAborted);
+            return NoContent();
+        }
     }
 }
