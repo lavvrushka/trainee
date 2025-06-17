728a06eeb5c200d6103ccd14e28574425dce6a83
Ivan Lauryu <urpretti@gmail.com>
2025-06-18 00:00:01 +0300

    changes3

---
+++ src/backend/DocumentsManagement/src/Application/DocumentsBusinessLogic/DTOs/Documents/DocumentDto.cs
@@ -0,0 +1,43 @@
+﻿using DocumentsDataAccess.Persistence.Entities;
+namespace DocumentsBusinessLogic.DTOs.Documents;
+
+public class DocumentDto
+{
+    public Guid Id { get; set; }
+    public string BlobUrl { get; set; } = null!;
+    public DateTime LastRetrievedAt { get; set; }
+    public bool IsDeleted { get; set; }
+}
+
+public static class DocumentMapper
+{
+    public static DocumentDto ToDto(this DocumentEntity documentEntity)
+    {
+        var dto = new DocumentDto
+        {
+            Id = documentEntity.Id,
+            BlobUrl = documentEntity.BlobUrl,
+            LastRetrievedAt = documentEntity.LastRetrievedAt
+        };
+
+        return dto;
+    }
+
+    public static DocumentEntity ToEntity(this CreateDocumentRequest createDocumentRequest)
+    {
+        var entity = new DocumentEntity
+        {
+            Id = Guid.NewGuid(),
+            BlobUrl = createDocumentRequest.BlobUrl,
+            LastRetrievedAt = DateTime.UtcNow,
+            IsDeleted = false
+        };
+
+        return entity;
+    }
+
+    public static void ApplyUpdate(this UpdateDocumentRequest updateDocumentRequest, DocumentEntity documentEntity)
+    {
+        documentEntity.BlobUrl = updateDocumentRequest.BlobUrl;
+    }
+}
---
+++ src/backend/DocumentsManagement/src/Application/DocumentsBusinessLogic/DTOs/Images/ImageDto.cs
@@ -0,0 +1,44 @@
+﻿using DocumentsDataAccess.Persistence.Entities;
+namespace DocumentsBusinessLogic.DTOs.Images;
+
+public class ImageDto
+{
+    public Guid Id { get; set; }
+    public string BlobUrl { get; set; } = null!;
+    public DateTime LastRetrievedAt { get; set; }
+    public bool IsDeleted { get; set; }
+}
+
+public static class ImageMapper
+{
+    public static ImageDto ToDto(this ImageEntity imageEntity)
+    {
+        var dto = new ImageDto
+        {
+            Id = imageEntity.Id,
+            BlobUrl = imageEntity.BlobUrl,
+            LastRetrievedAt = imageEntity.LastRetrievedAt,
+            IsDeleted = imageEntity.IsDeleted
+        };
+
+        return dto;
+    }
+
+    public static ImageEntity ToEntity(this CreateImageRequest createImageRequest)
+    {
+        var entity = new ImageEntity
+        {
+            Id = Guid.NewGuid(),
+            BlobUrl = createImageRequest.BlobUrl,
+            LastRetrievedAt = DateTime.UtcNow,
+            IsDeleted = false
+        };
+
+        return entity;
+    }
+
+    public static void ApplyUpdate(this UpdateImageRequest updateImageRequest, ImageEntity imageEntity)
+    {
+        imageEntity.BlobUrl = updateImageRequest.BlobUrl;
+    }
+}
